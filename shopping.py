7f53944435af1f8451768ff8b9bcc92878d43c08
Oyiwodu1 <phatewodu@gmail.com>
2026-09-10 14:27:16 +0100

    practice_repo

--- shopping.py
+++ shopping.py
@@ -6,19 +6,21 @@ shop ={
 }
 cart = ["bread", "egg","rice", "egg"]
 
-total = 0
 
-for items in cart:
-    print("")
-    if items in shop:
-        print(f"{items}: {shop[items]}") 
-    total += shop[items]
+def calculate_total(carts):
+    total = 0
+
+    for items in cart:
+        print("")
+        if items in shop:
+            print(f"{items}: {shop[items]}") 
+        total += shop[items]
+    print("you purchased", end=" ")
+    print(len(set(cart)), "items")    
+    return total
+total = calculate_total(cart)
 print(f"Total: {total}")
-print(len(set(cart)))
 if total > 5000:
     print("You qualify for a discount!")
 else:
     print("No discount")
-    
-    
-        
