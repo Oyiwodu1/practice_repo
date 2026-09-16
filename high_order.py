2d69ae96193365b9af8aafc3dc8e6d1b8153d182
Oyiwodu1 <phatewodu@gmail.com>
2026-09-16 16:17:06 +0100

    high_order.py

--- high_order.py
+++ high_order.py
@@ -8,10 +8,16 @@ def say_goodbye(name):
 say_goodbye("Faith")
 
 
-def run_function(greeting):
-    greeting("Favour")
-run_function(greeting)  #high-order function
+def run_function(say_goodbye):
+    say_goodbye("Favour")
+run_function(say_goodbye)  #high-order function
 
 #  A higher-order function is a function that takes 
 #  another function as an argument or returns a function
 
+def outer():
+    def inner():
+        print("Hello Faith")
+    return inner
+result = outer()
+result() 
